ab70ab45d97071c4439e6bbbafa19f77631e0dcc
Rafi Alghifari <raffialg11@gmail.com>
2023-04-11 15:04:05 +0700

    Create User Controller

---
+++ controllers/userControllers.go
@@ -0,0 +1,43 @@
+package controllers
+
+import (
+	"net/http"
+	"product-api/database"
+	"product-api/helpers"
+	"product-api/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	appJSON = "application/json"
+)
+
+func UserRegister(c *gin.Context) {
+	db := database.GetDB()
+	contentType := helpers.GetContentType(c)
+	_, _ = db, contentType
+	User := models.User{}
+
+	if contentType == appJSON {
+		c.ShouldBindJSON(&User)
+	} else {
+		c.ShouldBind(&User)
+	}
+
+	err := db.Debug().Create(&User).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"id":        User.ID,
+		"email":     User.Email,
+		"full_name": User.FullName,
+	})
+}
